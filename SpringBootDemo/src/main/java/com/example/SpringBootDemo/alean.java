2160ae54619a4392f40612e5dad73780d7924bc7
Vaishnavi <vaishnavipatil.9333@gmail.com>
2025-06-15 23:45:19 +0530

    kkkkk

---
+++ SpringBootDemo/src/main/java/com/example/SpringBootDemo/alean.java
@@ -0,0 +1,9 @@
+package com.example.SpringBootDemo;
+
+import org.springframework.stereotype.Component;
+@Component
+
+public class alean {
+    public void code(){
+    System.out.println("coding");}
+}
